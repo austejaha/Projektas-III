383c26aa8a3e67d096266e83e15f9817cdd4b0d6
Austėja Valeikaitė <81626144+austejaha@users.noreply.github.com>
2021-12-23 14:03:31 +0200

    Update projektas.cpp

--- projektas.cpp
+++ projektas.cpp
@@ -1,9 +1,6 @@
-/**
-* @file projektas.cpp
-* Vektoriaus pagrindinis failas. Aprasyta main funkcija. 
-*/
-
 #include "funkcijos.h"
+
+
 
 int main()
 {
@@ -13,14 +10,14 @@ int main()
     string failas = "";
     srand(time(0));
     cout << "Ar norite duomenis ivesti patys (kitu atveju duomenys bus nuskaityti is failo)? (t/n) ";
-    if(patvirtinimas()) pagalbine(St);
+    if (patvirtinimas()) pagalbine(St);
     else {
         generavimas(pasirinkimas(), failas);
         nuskaitymas(St, failas);
     }
     vidMed(St);
     cout << "Ar norite studentu rusiavima vykdyti 1 - aja strategija (kitu atveju bus vykdoma 2 - oji strategija)? (t/n) ";
-    if(patvirtinimas()) {
+    if (patvirtinimas()) {
         skirstymas1(St, Vargsai, Genijai);
         spausdinimas(Vargsai, "vargsai.txt");
         spausdinimas(Genijai, "genijai.txt");
@@ -28,7 +25,7 @@ int main()
 
     else {
         cout << "Ar norite naudoti optimizuota strategija (kitu atveju bus naudojama neoptimizuota strategija)? (t/n) ";
-        if(patvirtinimas()) skirstymas2(St, Genijai);
+        if (patvirtinimas()) skirstymas2(St, Genijai);
         else skirstymas3(St, Genijai); //neoptimizuotas
         spausdinimas(St, "vargsai.txt");
         spausdinimas(Genijai, "genijai.txt");
